5534f5c3c16f49ac0610b79036bbe88560fa015a
joppincal <106348367+joppincal@users.noreply.github.com>
2024-01-01 22:07:33 +0900

    ツールバー・ステータスバーの素を作成
    中身はまだまだ

--- soroeditor/MainWindow.py
+++ soroeditor/MainWindow.py
@@ -1,6 +1,7 @@
 from PySide6 import QtCore, QtWidgets
+from PySide6.QtCore import Qt
 from PySide6.QtGui import QAction, QKeySequence
-from PySide6.QtWidgets import QPlainTextEdit, QScrollBar, QWidget, QMainWindow
+from PySide6.QtWidgets import QMainWindow, QPlainTextEdit, QScrollBar, QToolBar, QToolButton, QWidget
 from random import randint
 
 from soroeditor import __global__ as g
@@ -15,6 +16,7 @@ class MainWindow(QMainWindow):
     def makeLayout(self):
         self.makeTextEditor()
         self.makeMenu()
+        self.makeToolBar()
         self.setCentralWidget(g.textEditor)
 
     def makeMenu(self):
@@ -86,6 +88,63 @@ class MainWindow(QMainWindow):
                 ]
             )
 
+    def makeToolBar(self):
+        toolBarSettings = {0:{'area':'Top', 'names':['NewFile', 'Save', 'SaveAs']}, 1:{'area':'Bottom', 'names':['Setting']}, 2:{'area':'Left', 'names':['Import', 'Export']}, 3:{'area':'Bottom', 'names':['CurrentPlace']}} # temporary
+        toolButtonsElements = {
+            'NewFile':{'text':'新規作成', 'icon':None, 'action':None},
+            'OpenFile':{'text':'ファイルを開く', 'icon':None, 'action':None},
+            'Save':{'text':'上書き保存', 'icon':None, 'action':None},
+            'SaveAs':{'text':'名前をつけて保存', 'icon':None, 'action':None},
+            'Reload':{'text':'再読込', 'icon':None, 'action':None},
+            'ProjectSetting':{'text':'プロジェクト設定', 'icon':None, 'action':None},
+            'Setting':{'text':'設定', 'icon':None, 'action':None},
+            'Search':{'text':'検索', 'icon':None, 'action':None},
+            'Replace':{'text':'置換', 'icon':None, 'action':None},
+            'Import':{'text':'インポート', 'icon':None, 'action':None},
+            'Export':{'text':'エクスポート', 'icon':None, 'action':None},
+            'Template':{'text':'定型文', 'icon':None, 'action':None},
+            'Bookmark':{'text':'付箋', 'icon':None, 'action':None},
+            'Undo':{'text':'取り消し', 'icon':None, 'action':None},
+            'Repeat':{'text':'取り消しを戻す', 'icon':None, 'action':None},
+
+            'CurrentPlace':{'text':'カーソルの現在位置', 'icon':None, 'action':None},
+            'HotKeys1':{'text':'[Ctrl+O]: 開く  [Ctrl+S]: 上書き保存  [Ctrl+Shift+S]: 名前をつけて保存  [Ctrl+R]: 最後に使ったファイルを開く（起動直後のみ）', 'icon':None, 'action':None},
+            'HotKeys2':{'text':'[Enter]: 1行追加(下)  [Ctrl+Enter]: 1行追加(上)  [Shift+Enter]: 通常改行  [Ctrl+Z]: 取り消し  [Ctrl+Shift+Z]: 取り消しを戻す  [Ctrl+F]: 検索  [Ctrl+Shift+F]: 置換', 'icon':None, 'action':None},
+            'HotKeys3':{'text':'[Ctrl+Q][Alt+Q][Alt+<]: 左に移る  [Ctrl+W][Alt+W][Alt+>]: 右に移る', 'icon':None, 'action':None},
+            'infomation':{'text':'各機能情報', 'icon':None, 'action':None},
+            'kaomoji':{'text':'顔文字', 'icon':None, 'action':None},
+            'StatusBarMessage':{'text':'ステータスバー初期メッセージ', 'icon':None, 'action':None},
+            'Clock':{'text':'時計', 'icon':None, 'action':None},
+            'CountDown':{'text':'カウントダウン', 'icon':None, 'action':None},
+            'StopWatch':{'text':'ストップウォッチ', 'icon':None, 'action':None},
+        }
+        toolButtonStyle = 'IconOnly' # temporary
+        toolButtonStyle = getattr(Qt.ToolButtonStyle, f'ToolButton{toolButtonStyle}')
+        g.toolBars = {}
+
+        for i, toolBarSetting in toolBarSettings.items():
+            toolBar = QToolBar()
+            toolBar.setToolButtonStyle(toolButtonStyle)
+            area = getattr(Qt.ToolBarArea, f'{toolBarSetting["area"]}ToolBarArea')
+            self.addToolBarBreak(area)
+            self.addToolBar(area, toolBar)
+
+            toolButtons = []
+            for name in toolBarSetting['names']:
+                elements = toolButtonsElements[name]
+                toolButton = QToolButton()
+                toolButton.setText(elements['text'])
+                if elements['icon']:
+                    toolButton.setIcon(elements['icon'])
+                if elements['action']:
+                    toolButton.addAction(elements['action'])
+                toolBar.addWidget(toolButton)
+                toolButtons.append(toolButton)
+
+            g.toolBars[i] = {}
+            g.toolBars[i]['toolbar'] = toolBar
+            g.toolBars[i]['toolbutton'] = toolButtons
+
     def makeTextEditor(self):
         g.textEditor = TextEditor(self)
 
@@ -99,14 +158,15 @@ class TextEditor(QWidget):
         g.textBoxes = [QPlainTextEdit() for _ in range(3)]
 
         # textboxにテキストを設定(仮)
-        for textbox in g.textBoxes:
+        for textBox in g.textBoxes:
             text = ''.join([f'{i}'+'a'*randint(1, 100)+'\n' for i in range(randint(1, 30))])[:-1]
-            textbox.setPlainText(text)
+            textBox.setPlainText(text)
 
         # textboxにSlotを設定
-        for textbox in g.textBoxes:
-            textbox.verticalScrollBar().valueChanged.connect(self.textBoxScrollBarValueChanged)
-            textbox.verticalScrollBar().setVisible(False)
+        for textBox in g.textBoxes:
+            textBox.verticalScrollBar().valueChanged.connect(self.textBoxScrollBarValueChanged)
+            textBox.verticalScrollBar().setVisible(False)
+            textBox.cursorPositionChanged.connect(self.cursorPositionChanged)
 
         global mainScrollBar
         mainScrollBar = QScrollBar()
@@ -116,8 +176,8 @@ class TextEditor(QWidget):
 
         self.hlayout = QtWidgets.QHBoxLayout(self)
 
-        for textbox in g.textBoxes:
-            self.hlayout.addWidget(textbox)
+        for textBox in g.textBoxes:
+            self.hlayout.addWidget(textBox)
         self.hlayout.addWidget(mainScrollBar)
 
         self.addReturn()
