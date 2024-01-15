529fe27a95661617a1c2f85f8b2ec6c4ac3ec10e
joppincal <106348367+joppincal@users.noreply.github.com>
2024-01-15 18:28:07 +0900

    Update SettingWindow.py
    設定ウィンドウの基礎を追加した

--- soroeditor/SettingWindow.py
+++ soroeditor/SettingWindow.py
@@ -1,10 +1,9 @@
-from typing import Optional
-from PySide6 import QtWidgets
-from PySide6.QtGui import QFont
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QLabel, QWidget
+from PySide6.QtCore import Qt, Slot
+from PySide6.QtWidgets import (QDialogButtonBox, QHBoxLayout, QVBoxLayout,
+                               QWidget)
 
-from soroeditor.Icon import Icon
+from soroeditor import SettingOperation
+
 
 class SettingWindow(QWidget):
     def __init__(self, parent: QWidget) -> None:
@@ -12,4 +11,41 @@ class SettingWindow(QWidget):
         self.resize(800, 600)
         self.setWindowFlags(Qt.Dialog)
         self.setWindowModality(Qt.WindowModality.ApplicationModal)
-        self.setWindowTitle('SoroEditor - 設定')
+        self.setWindowTitle("SoroEditor - 設定")
+        self.makeLayout()
+
+    def makeLayout(self):
+        global bottomBar
+        vBox = QVBoxLayout(self)
+        hBox = QHBoxLayout()
+        bottomBar = QDialogButtonBox()
+        vBox.addLayout(hBox)
+        vBox.addWidget(bottomBar)
+        vBox1 = QVBoxLayout()
+        vBox2 = QVBoxLayout()
+        hBox.addLayout(vBox1, 1)
+        hBox.addLayout(vBox2, 1)
+        vBox1.addWidget(QWidget())
+        vBox2.addWidget(QWidget())
+
+        bottomBar.setStandardButtons(
+            bottomBar.StandardButton.Save | bottomBar.StandardButton.Cancel
+        )
+        bottomBar.button(bottomBar.StandardButton.Save).setText("保存(&S)")
+        bottomBar.button(bottomBar.StandardButton.Cancel).setText("キャンセル")
+        bottomBar.button(bottomBar.StandardButton.Cancel)
+
+        for button in bottomBar.buttons():
+            button.clicked.connect(self.buttonClicked)
+
+    @Slot()
+    def buttonClicked(self):
+        button = self.sender()
+        if button == bottomBar.button(bottomBar.StandardButton.Save):
+            self.saveSetting()
+        if button == bottomBar.button(bottomBar.StandardButton.Cancel):
+            self.close()
+
+    def saveSetting(self):
+        data = SettingOperation.DEFAULTSETTINGDATA  # 本来は、ウィンドウ内の各項目を読み取るべし
+        return SettingOperation.writeSettingFile(data)
