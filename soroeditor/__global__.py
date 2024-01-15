7901eef4ae317a6302bce8791ee395bc95b9a8ac
joppincal <106348367+joppincal@users.noreply.github.com>
2024-01-15 22:19:39 +0900

    Update __global__.py
    __global__.pyに型ヒントを記述

---
+++ soroeditor/__global__.py
@@ -0,0 +1,7 @@
+from logging import Logger
+from soroeditor.MainWindow import TextEditor, PlainTextEdit, LineEdit
+
+logger: Logger
+textEdits: list[PlainTextEdit]
+lineEdits: list[LineEdit]
+textEditor: TextEditor
