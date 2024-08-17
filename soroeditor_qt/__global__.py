e856a426f74b83d7db3a756d44d44baa3708c1a5
joppincal <106348367+joppincal@users.noreply.github.com>
2024-08-17 17:22:54 +0900

    __global__.toolBars を削除
    元々使用されていないものであった。削除

--- soroeditor_qt/__global__.py
+++ soroeditor_qt/__global__.py
@@ -1,8 +1,5 @@
-from PySide6.QtWidgets import QToolBar
-
 from .MainWindow import LineEdit, PlainTextEdit, TextEditor
 
-toolBars: list[QToolBar]
 textEdits: list[PlainTextEdit]
 lineEdits: list[LineEdit]
 textEditor: TextEditor
